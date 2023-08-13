b08067bdd0295e4603db1060b979feae1d1ccafa
orgg92 <orgg92@gmail.com>
2023-08-13 22:33:56 +0100

    Added check on setting console size

--- Application/Handlers/FileArchiver/FileArchiverCommand.cs
+++ Application/Handlers/FileArchiver/FileArchiverCommand.cs
@@ -30,10 +30,6 @@
 
                 await _consoleService.WriteToConsole(message);
 
-                // if SOURCE VERSION OF THE DESTINATION FILE doesn't exist
-                // OR
-                // the DESTINATION FILE last modified is before the SOURCE FILE'S last modified
-
                 var destinationDirectory = SharedContent.ReplaceDriveToArchiveContext(SharedContent.DestinationDrive.ToCharArray()[0], new DirectoryInfo(request.FileName).Parent.ToString());
 
                 // check - if not exists, create
@@ -45,6 +41,10 @@
                 // get full destination filepath and create if not exists
                 var destPath = $"{destinationDirectory}\\{new DirectoryInfo(request.FileName).Name}";
                 var srcPath = request.FileName;
+
+                // if SOURCE VERSION OF THE DESTINATION FILE doesn't exist
+                // OR
+                // the DESTINATION FILE last modified is before the SOURCE FILE'S last modified
 
                 if (!File.Exists(destPath) ) // || (File.GetLastWriteTimeUtc(srcPath) > File.GetLastWriteTimeUtc(destPath)) )
                 {
